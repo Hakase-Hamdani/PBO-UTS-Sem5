69da903e009226dbaaa2c561aa4613aa0271ba21
Hakase-Hamdani <mhdhamdani.formal@gmail.com>
2023-10-18 21:14:43 +0800

    pertemuan 3 i guess

---
+++ src/praktikum01/Praktikum01.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package praktikum01;
+import forms.frameBahan;
+
+/**
+ *
+ * @author hakase
+ */
+public class Praktikum01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new frameBahan().setVisible(true);
+        
+        
+        // new koneksi();
+        
+        //koneksi obj = new koneksi();
+        //try {
+              //obj.perintah.execute("INSERT INTO bahan VALUE ('kd02', 'Tepung Terigu', 'makanan', '20')");
+              //obj.perintah.execute("UPDATE bahan set nama='Tepung Goreng', kategori='bahan mentah', qty='100' where id='kd01'");
+              //obj.perintah.execute("DELETE FROM bahan WHERE id = 'kd01'");
+              //String sql = "INSERT INTO bahan VALUE (?, ?, ?, ?)";
+              //String sql = "UPDATE bahan set nama=?, kategori=?, qty=? where id=?";
+        //      String sql = "DELETE FROM bahan WHERE id = ?";
+              //PreparedStatement kueri = obj.konekDB.prepareStatement(sql);
+              
+        //      kueri.setString(1, "kd01");
+              //kueri.setString(2, "minuman bersoda");
+              //kueri.setInt(3, 10);
+              //kueri.setString(4, "kd01");
+              
+        //      kueri.executeUpdate();
+        //} catch (SQLException ex) {
+        //    System.out.print(ex);
+        //}
+    }
+    
+}
